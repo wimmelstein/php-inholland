28df4b9a2e927d12f1133d49d4040f3fe58dbf5a
wimmelstein <wim.wiltenbrug@gmail.com>
2019-11-18 22:50:33 +0100

    Started canvas

--- src/index.php
+++ src/index.php
@@ -1,5 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Canvas</title>
 
-phpinfo();
+    <script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
+    <script src="js/canvas.js"></script>
 
-?>
+    <style>
+    canvas {
+        border: 1px solid black;
+    }
+    </style>
+</head>
+<body>
+    <canvas></canvas>
+</body>
+</html>
---
+++ src/js/canvas.js
@@ -0,0 +1,5 @@
+var canvas = document.getSelection('canvas');
+console.log(canvas);
+canvas.width = 600;
+canvas.height = 400;
+console.log(canvas);
