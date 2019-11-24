f2e6280d7bffcf71c66d7bb64471c7466f763b19
wimmelstein <wim.wiltenbrug@gmail.com>
2019-11-24 14:59:52 +0100

    First file manager

--- src/index.php
+++ src/index.php
@@ -1,5 +1,56 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-phpinfo();
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-?>
+  <link href="/css/style.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <title>File Manager</title>
+</head>
+
+<body>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">File manager</h1>
+      <p>PHP File manager</p>
+    </div>
+  </div>
+
+  <table class="table table-hover">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">name</th>
+        <th scope="col">type</th>
+      </tr>
+    </thead>
+    <tbody>
+
+      <?php
+      $dir = 'workdir';
+      $files1 = scandir($dir);
+      $files2 = scandir($dir, 1);
+
+      foreach ($files1 as $filename) {
+        ?>
+        <tr>
+          <td><?php echo $filename ?> </td>
+          <td><?php echo (is_dir($filename)) ? 'directory' : 'regular file'; ?></td>
+        </tr>
+
+      <?php
+      }
+      ?>
+    </tbody>
+  </table>
+  <div class="container-form" >
+    <form name="upload" method="post" action="upload.php" enctype="multipart/form-data">
+     <input type="file" class="form-control-file" name="fileToUpload"  id="fileToUpload">
+      <button type="submit" class="btn btn-primary">Upload</button>
+    </form>
+  </div>
+</body>
+</html>
