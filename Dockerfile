eeb51d0341ecc5b643ff9674dd394c0e17284a09
wimmelstein <wim.wiltenbrug@gmail.com>
2019-12-10 15:58:44 +0100

    Create a ticket to Wimmelsoft Codefest

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,9 @@ RUN a2enmod rewrite
 RUN echo "ServerName localhost" >> /etc/apache2/apache2.conf
 RUN sed -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
 
-RUN docker-php-ext-install mysqli
+RUN apt-get update && \
+    apt-get install -y \
+    zlib1g-dev libpng-dev
 
+RUN docker-php-ext-install mysqli 
+RUN docker-php-ext-install gd
--- src/index.php
+++ src/index.php
@@ -1,5 +1,49 @@
 <?php
 
-phpinfo();
+require 'model/mypdf.php';
+require 'model/user.php';
+require 'lib/phpqrcode.php';
 
+$user = new User(null, 'Wim', 'Wiltenburg', 51);
+
+$pdf = new PDF();
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Arial', '', 20);
+
+
+$pdf->Cell(0, 10, 'Wimmelsoft CodeFest', 0, 1, 'C');
+
+
+$pdf->SetFont('Arial', '', 12 );
+
+$pdf->Ln(30);
+
+$pdf->Cell(0, 10, 'Dear ' . $user->getUserName(), 0, 1, 'L');
+
+$pdf->Ln();
+
+$pdf->Cell(0, 10, 'This ticket is to grant access to Wimmelsoft Codefest to: ', 0, 1, 'L');
+
+$pdf->Ln();
+
+$pdf->Cell(0, 10, 'First name: ' . $user->getFirstName(), 0, 1, 'L');
+$pdf->Cell(0, 10, 'Last name: ' . $user->getLastName(), 0, 1, 'L');
+
+$pdf->Ln();
+
+$pdf->Cell(0,10, 'Date: ' . date('d-m-Y'), 0, 1, 'L');
+
+$pdf->Ln();
+
+$filename = 'output/' . str_replace(' ', '_', $user->getUserName()) . '.png';
+
+QRcode::png(md5($user->getUserName()), $filename, 'L', 4, 2);
+
+$pdf->Image($filename);
+
+$pdf->Output('F', 'output/ticket.pdf');
+
+// Remove image for privacy reasons
+unlink($filename);
 ?>
