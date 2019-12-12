443919aa455b37f4a349150f5d1c887e1be3336b
Wim Wiltenburg <wwiltenburg@qualogy.com>
2019-12-12 16:59:58 +0100

    First draft payments

--- src/index.php
+++ src/index.php
@@ -1,5 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="/css/style.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="Cache-control" content="no-cache" />
+
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script src="js/user.js"></script>
+
+  <title>Users REST API</title>
+</head>
+
+<body>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Payment</h1>
+      <p>Demonstration of Mollie Payment</p>
+    </div>
+  </div>
+
 <?php
 
-phpinfo();
+    $amount = new \stdClass();
+    $amount->currency = "EUR";
+    $amount->value = "10.00";
+
+    $payment = new \stdClass();
+    $payment->amount = $amount;
+    $payment->webhook = "https://wiltenburg.eu/process.php";
+
+    echo json_encode($payment);
+
 
 ?>
+
+
+
+
+</body>
+</html>
