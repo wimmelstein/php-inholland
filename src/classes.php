f9b7fe8b42aa52a7004dd6bfa99f76221a6c6de0
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2020-12-02 12:03:13 +0100

    End tag for classes.php

--- src/classes.php
+++ src/classes.php
@@ -3,3 +3,5 @@ include $_SERVER['DOCUMENT_ROOT'] . '/util/db.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/model/user.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/service/UserService.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/util/common.php';
+
+?>
