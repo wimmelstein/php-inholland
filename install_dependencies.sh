0062c09aac7c612c845532c0415da7182ac12f0e
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-03-03 14:02:36 +0100

    Added package-lock.json

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,6 @@
 # Go to the correct directory
 # shellcheck disable=SC2155
-export projectdir=`pwd`
+export projectdir=$(pwd)
 export scriptdir="$projectdir/src"
 # shellcheck disable=SC2164
 cd "$scriptdir"
