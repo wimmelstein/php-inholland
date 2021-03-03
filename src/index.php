7eb97b4d85ba02af849df9a37ce6bd7609b7ead4
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-03-03 12:34:56 +0100

    Working node_module

--- src/index.php
+++ src/index.php
@@ -1,35 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Canvas</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Canvas</title>
 
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> 
-        <script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        
-    </head>
-    <body>
-    <header>
+</head>
+<body>
+<header>
     <div class="jumbotron  jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">Signature</h1>
-        <p>Signature Pad by <a href="https://github.com/szimek/signature_pad">szimek</a></p>
-      </div>
+        <div class="container">
+            <h1 class="display-4">Signature</h1>
+            <p>Signature Pad by <a href="https://github.com/szimek/signature_pad">szimek</a></p>
+        </div>
     </div>
 
-    </header>
-    <section id="body">
-        <canvas id="canvas"></canvas>
-        <script src="js/canvas.js"></script>
-        <div id="buttons">
-            <button type="submit" class="btn btn-primary" onClick="saveImage()">Save</button>
-            <button type="submit" class="btn btn-secondary" onClick="reset()">Reset</button>
+</header>
+<section id="body">
+    <canvas id="canvas"></canvas>
+    <script type='module' src="js/canvas.js"></script>
+    <div id="buttons">
+        <button type="submit" id="saveImage" class="btn btn-primary">Save</button>
+        <button type="submit" id="resetImage" class="btn btn-secondary" onClick="reset()">Reset</button>
 
-    </section>    
-    </body>
+</section>
+</body>
 </html>
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,5 +1,11 @@
-import SignaturePad from 'signature_pad';
+import SignaturePad from '/node_modules/signature_pad/dist/signature_pad.js';
 
+window.addEventListener('load', function () {
+    const saveButton = document.getElementById('saveImage');
+    saveButton.addEventListener('click', saveImage);
+    const resetButton = document.getElementById('resetImage');
+    resetButton.addEventListener('click', reset);
+})
 const canvas = document.getElementById("canvas");
 canvas.height = 250;
 canvas.width = 700;
