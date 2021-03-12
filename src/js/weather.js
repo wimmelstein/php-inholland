bfc47d2e6a928394fb3e60863cec04146328999f
wimmelstein <wim.wiltenburg@gmail.com>
2021-03-12 20:58:55 +0100

    Small refactor

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,21 +1,21 @@
 navigator.geolocation.getCurrentPosition(pos => {
+        const KELVIN = 273.15;
         const latitude = pos.coords.latitude;
         const longitude = pos.coords.longitude;
-        let apiKey;
 
         fetch('http://localhost/api-key.php')
             .then(function (response) {
                 return response.json()
             })
             .then(function (data) {
-                apiKey = data.API_KEY;
+                const apiKey = data.API_KEY;
                 url = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&lat=${latitude}&lon=${longitude}`;
                 fetch(url)
                     .then(response => response.json())
                     .then((data) => {
                         const locationDiv = document.getElementById('location');
-                        const temperature = Math.round(data.main.temp - 275.15, 2);
-                        const feelsLike = Math.round(data.main.feels_like - 275.15, 2);
+                        const temperature = Math.round((data.main.temp - KELVIN), 2);
+                        const feelsLike = Math.round((data.main.feels_like - KELVIN), 2);
                         const description = data.weather[0].description;
                         const locationName = data.name;
                         locationDiv.innerHTML = `${locationName}: ${description}: ${temperature}&#176;: feels like ${feelsLike} `;
